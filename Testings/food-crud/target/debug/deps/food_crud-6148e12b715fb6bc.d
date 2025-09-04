74bee4e9506e91d686c1dbb8b68d6f70335ae43f
icarus vagner <ruthakaza@gmail.com>
2025-09-04 14:13:29 +0800

    Delete Testings/food-crud/target directory

--- Testings/food-crud/target/debug/deps/food_crud-6148e12b715fb6bc.d
+++
@@ -1,5 +0,0 @@
-/home/ricklance/Projects/Testings/food-crud/target/debug/deps/food_crud-6148e12b715fb6bc: src/main.rs
-
-/home/ricklance/Projects/Testings/food-crud/target/debug/deps/food_crud-6148e12b715fb6bc.d: src/main.rs
-
-src/main.rs:
--- Testings/food-crud/target/debug/food-crud.d
+++
@@ -1,1 +0,0 @@
-/home/ricklance/Projects/Testings/food-crud/target/debug/food-crud: /home/ricklance/Projects/Testings/food-crud/src/main.rs
